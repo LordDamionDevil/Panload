994fc19c93ed1c1227a2b0efd46d6d68ba5613f0
Walter <Sphinxs@users.noreply.github.com>
2017-07-05 12:53:07 -0300

    Version 0.1

---
+++ Compile.py
@@ -0,0 +1,7 @@
+
+import sys
+
+from cx_Freeze import setup, Executable
+
+
+setup ( name = 'Panload', version = '1.1', description = 'Download the galaxy', executables = [ Executable ( 'Panload.py' ) ] )
