06db06096362132371e721341debb01a742297fa
Sphinxs <waltim@outlook.com>
2017-07-05 02:18:03 -0300

    Version 0.0

---
+++ Panload.py
@@ -0,0 +1,151 @@
+
+# -*- coding: utf-8 -*-
+
+import os
+
+import sys
+
+import requests
+
+import subprocess
+
+
+def clear () :
+
+    # Stackoverflow.com/questions/16242025/term-environment-variable-not-set
+
+    subprocess.call ( 'clear', shell = True )
+
+
+class Archive ( object ) :
+
+    def __init__ ( self ) :
+
+        pass
+
+    # Name, Url, Extension
+
+    def download ( self, name = None, url = None, ext = None ) :
+
+        ''' Panload.download ( 'Wallpaper', 'i.imgur.com/csDLxo9.jpg', 'jpg' ) '''
+
+        if name == None or url == None or ext == None :
+
+            clear ()
+
+            print ( '\nError - Parameters\n' )
+
+            sys.exit ( 1 )
+
+        self.name = name
+
+        self.url = url if url.startswith ( 'https://' ) or url.startswith ( 'http://') else 'http://' + url
+
+        self.ext = ext if ext.startswith ( '.' ) else '.' + ext
+
+        requ = requests.get ( self.url )
+
+        with open ( self.name + self.ext, 'wb' ) as arch :
+
+            for chunk in requ.iter_content ( chunk_size = 255 ) :
+
+                if chunk :
+
+                    arch.write ( chunk )
+
+        print ( '\n\nDownload done - {0}\n'.format (name) )
+
+    # V = Video, A = Audio & F = Final
+
+    def combine ( self, vname, vurl, vext, aname, aurl, aext, fname, fext ) :
+
+        ''' Panload.combine ( 'Vid', 'bit.ly/2so2bjW', 'mp4', 'Aud', 'bit.ly/2tKdLt6', 'mp3', 'Anime', 'mp4' ) '''
+
+        self.download ( vname, vurl, vext )
+
+        self.download ( aname, aurl, aext )
+
+        comand = 'ffmpeg -i {0}.{1} -i {2}.{3} -vcodec copy -acodec copy {4}.{5} && rm -rf {0}.{1} {2}.{3}'.format ( vname, vext, aname, aext, fname, fext )
+
+        subprocess.call ( comand, shell = True )
+
+        clear ()
+
+        print ( '\n\nDownload done - {0}\n'.format (fname) )
+
+
+Panload = Archive ()
+
+
+while True :
+
+    clear ()
+
+    print ( '\nMenu :\n\n\t1 - Download\n\n\t2 - Concatenação\n')
+
+    choose = 0
+
+    try :
+
+        choose = int ( input ( 'Informe sua escolha : ' ) )
+
+    except :
+
+        continue
+
+    if choose == 1 :
+
+        clear ()
+
+        print ( '\nDownload :')
+
+        dnome = input ( '\n\tInforme um nome para o arquivo : ' )
+
+        durl = input ( '\n\tInforme a url do arquivo : ' )
+
+        dext = input ( '\n\tInforme a extensão do arquivo : ')
+
+        Panload.download ( dnome, durl, dext )
+
+    elif choose == 2 :
+
+        clear ()
+
+        print ( '\nConcatenação :')
+
+        vnome = input ( u'\n\tInforme um nome para o vídeo : ' )
+
+        vurl = input ( u'\n\tInforme a url do vídeo : ' )
+
+        vext = input ( u'\n\tInforme a extensão do vídeo : ' )
+
+        aname  = input ( u'\n\n\tInforme um nome para o áudio : ' )
+
+        aurl = input ( u'\n\tInforme a url do áudio : ' )
+
+        aext = input ( u'\n\tInforme a extensão áudio : ' )
+
+        fname = input ( u'\n\n\tInforme o nome do arquivo final : ' )
+
+        fext = input ( u'\n\tInforme a extensão do arquivo final : ' )
+
+
+        Panload.combine ( vnome, vurl, vext, aname, aurl, aext, fname, fext )
+
+    else :
+
+        print ( '\nOpção invalida' )
+
+    out = input ( '\nDigite 0 para sair ou outro digito para continuar : ' )
+
+    if ( out == '0' ) :
+
+        clear ()
+
+        sys.exit (0)
+
+    else :
+
+        clear ()
+
+        continue
---
+++ README.md
@@ -0,0 +1,49 @@
+
+<h2>[Pan](https://pt.wikipedia.org/wiki/Pandora)[load](https://pt.wikipedia.org/wiki/Load)<h2>
+
+![Panload](http://i.imgur.com/Ys0GJtw.png "Panload")
+
+Aplicação [Cli](https://en.wikipedia.org/wiki/Command-line_interface) em [Python](https://www.python.org/) que realiza o download de arquivos de diversas extensões além de realizar o download de um vídeo e áudio e concatenar os mesmos. :milky_way:
+
+
+#### Dependências
+
+As libs [os](https://docs.python.org/3.7/library/os.html) e [sys](https://docs.python.org/3.7/library/sys.html) por padrão vem no interpretador Python ( Built-in ), além destas duas libs são requeridas : ( Instalação com preferência em Python 3 ou seja `pip3 install ...` )
+
+[Sub Process](https://docs.python.org/3.7/library/subprocess.html)
+
+&
+
+[Requests](http://docs.python-requests.org/en/master/)
+
+Também é necessário instalar o [FFmpe](https://ffmpeg.org/) caso você deseje concatenar vídeo e áudio, o mesmo tem um guia de instalação para Mac, Windows & Linux nesta página **[aqui](https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg)**.
+
+> Player : [Vlc Player](http://www.videolan.org/)
+
+#### Execução
+
+A execução se da de forma simples, basta realizar o `git clone` deste repositório, acessar o mesmo `cd Panload` e interpretar o arquivo `Panload.py` de preferência em `python3 Panload.py`. Os arquivos baixados serão salvos no diretório atual em que o arquivo `Panload.py` se encontra, vale avisar que caso você tenha realizado o download de um áudio com nome 'x' ou um vídeo com nome ' y ' ( Ou ambos ) e realize a e concatenação de um vídeo + áudio com o mesmo nome dos arquivos já baixados a exclusão de ' x ' e ' y ' vai ser realizada. Para desativar a exclusão edite a linha `68` removendo o seguinte trecho de código : `&& rm -rf {0}.{1} {2}.{3}`.
+
+#### Motivação
+
+A principal motivação e uso desta aplicação se faz presente pelo site [Coub](http://coub.com/explore/hot) que tem uma falta imensa de opções ( Que funcionem ) para realizar o download de vídeos / áudios da plataforma, então caso deseje realizar o download de um ' Coub ' acesse a página do mesmo e pressione `ctrl + u`, `ctrl + f` e digite **mp3** para encontrar o link do áudio ou **mp4** para encontrar o link do vídeo, então copie tal link e cole o mesmo na linha de comando desta aplicação ( *Panload* ). - Ps : O download de qualquer arquivo por parte dessa aplicação ( Até o momento ) necessita que a extensão de tal arquivo esteja implícita em sua [Uri](https://en.wikipedia.org/wiki/Uniform_Resource_Identifier) ( Url ) !
+
+![Coub Download](http://i.imgur.com/JVvukrY.png "Download And Save Coub Videos")
+
+#### Issues
+
+* Inferência na obtenção das url's de sites como [Sound Cloud](https://soundcloud.com/), [Youtube](https://www.youtube.com/) & [Coub](http://coub.com/explore/hot)
+
+* Verificação de arquivos locais na concatenação
+
+* Escolha de diretório
+
+* Compressor para os arquivos
+
+* Implementação de Gui
+
+#### Tratar
+
+* Parâmetros errados
+
+* Padronização do idioma ( Inglês )
